657d32d9510d8bb22cb22c2903989bc4981a1d50
shilpadm1998 <shilpadm434@gmail.com>
2021-11-06 17:05:59 +0530

    Even Numbers

---
+++ Web-Automation/src/com/sgtestin/testscript/EvenNumber.java
@@ -0,0 +1,18 @@
+package com.sgtestin.testscript;
+
+public class EvenNumber {
+
+	public static void main(String[] args)
+	{
+		for(int i=1;i<=10;i++)
+		{
+			if(i%2==0)
+			{
+				System.out.println("Even Number is "+i);
+			}
+		}
+		
+
+	}
+
+}
